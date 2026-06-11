b14b57944f678bcd87c0482e05b211807c6afbd6
Bet do Interior <nationgladiators@gmail.com>
2026-06-11 18:50:28 -0300

    Update page.tsx

--- page.tsx
+++ page.tsx
@@ -1,1 +1,19 @@
+import Header from "@/components/Header";
 
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black">
+
+      <Header />
+
+      <div className="flex justify-center items-center h-[500px]">
+
+        <h1 className="text-5xl text-green-500 font-bold">
+          BET DO INTERIOR
+        </h1>
+
+      </div>
+
+    </main>
+  );
+}
